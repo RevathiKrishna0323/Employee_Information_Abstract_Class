84141f841b2071a34d2b96038b0520c417609485
Revathi G <revathikrishna0323@gmail.com>
2022-10-13 08:49:44 +0530

    created constructor, getters, setters to the attributes to  the Manager.java

--- src/main/java/com/io/github/Manager.java
+++ src/main/java/com/io/github/Manager.java
@@ -10,4 +10,30 @@ public class Manager extends Employee {
     private int teamSize;
     private int teamRatings;
 
+    public Manager(int teamSize, int teamRatings) {
+        this.teamSize = teamSize;
+        this.teamRatings = teamRatings;
+    }
+
+    public Manager(String employeeName, String code, String dateOfBirth, String age, String salary, int teamSize, int teamRatings) {
+        super(employeeName, code, dateOfBirth, age, salary);
+        this.teamSize = teamSize;
+        this.teamRatings = teamRatings;
+    }
+
+    public int getTeamSize() {
+        return teamSize;
+    }
+
+    public void setTeamSize(int teamSize) {
+        this.teamSize = teamSize;
+    }
+
+    public int getTeamRatings() {
+        return teamRatings;
+    }
+
+    public void setTeamRatings(int teamRatings) {
+        this.teamRatings = teamRatings;
+    }
 }
